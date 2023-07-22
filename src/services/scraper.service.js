18f53bc941b1d2eacd9edbd2ecdc6ef1edf17526
José <mailjmq@gmail.com>
2023-07-22 15:44:47 -0300

    scrapped date and price successfully

--- src/services/scraper.service.js
+++ src/services/scraper.service.js
@@ -3,44 +3,60 @@ import axios from "axios";
 import * as fs from "fs";
 import puppeteer from "puppeteer";
 
-const url = "https://www.tradingview.com/symbols/ICEEUR-BRN1!/";
+const url =
+	"https://www.cmegroup.com/markets/energy/crude-oil/brent-crude-oil-last-day.settlements.html";
 
-const selector =
-	"#js-category-content > div.tv-category-symbol-header > div.js-symbol-page-header-root > div > div > div > div.quotesRow-pAUXADuj > div:nth-child(1) > div > div.lastContainer-JWoJqCpY > span.last-JWoJqCpY.js-symbol-last > span";
+const dateSelectorText =
+	"#productTabData > div > div > div > div > div > div:nth-child(2) > div > div > div > div > div > div.trade-date-row.row > div > div > div > button > span";
+
+const priceSelectorText =
+	"#productTabData > div > div > div > div > div > div:nth-child(2) > div > div > div > div > div > div:nth-child(8) > div > div > div > div > table > tbody > tr:nth-child(1) > td:nth-child(7)";
 
 async function getInnerText(url, selector) {
-	const browser = await puppeteer.launch();
+	// Launch the browser and open a new blank page
+	const browser = await puppeteer.launch({
+		headless: "new",
+	});
+
 	const page = await browser.newPage();
 	await page.goto(url);
+	await page.setViewport({ width: 1080, height: 1024 });
 
-	const texto = await page.evaluate((selector) => {
-		const elemento = document.querySelector(selector);
-		return elemento.innerText;
-	}, selector);
+	// Locate the date and price selectors
+	const dateSelector = await page.waitForSelector(dateSelectorText);
+	const priceSelector = await page.waitForSelector(priceSelectorText);
 
+	// Get content of date and price selectors
+	const dateContent = await dateSelector?.evaluate((el) => el.textContent);
+	const priceContent = await priceSelector?.evaluate((el) => el.textContent);
+
+	// Print full date and price
+	console.log("The date of this blog post is %s.", dateContent);
+	console.log("The price of this blog post is %s.", priceContent);
+
+	// Close the browser
 	await browser.close();
 
-	return texto;
+	// Return full response
+	return { date: dateContent, price: priceContent };
 }
-
-const getYesterdayDate = () => {
-	const today = new Date();
-	const yesterday = new Date(today);
-	yesterday.setDate(yesterday.getDate() - 1);
-	return yesterday.toISOString().slice(0, 10);
-};
 
 class ScraperService {
 	constructor() {
 		this.url = url;
-		this.selector = selector;
+		this.priceSelector = priceSelectorText;
+		this.dateSelector = dateSelectorText;
 	}
 
 	async getScrapedPrice() {
+		const data = await getInnerText(this.url, this.selector);
+
 		const price = {
-			date: getYesterdayDate(),
-			price: Number(await getInnerText(this.url, this.selector)),
+			date: new Date(data.date),
+			price: Number(data.price),
 		};
+
+		console.log(price);
 
 		return price;
 	}
