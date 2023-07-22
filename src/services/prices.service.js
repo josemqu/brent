da34cea51b0b648e234dce46919717e8c8ad83c1
José <mailjmq@gmail.com>
2023-07-22 18:49:51 -0300

    nice style added

--- src/services/prices.service.js
+++ src/services/prices.service.js
@@ -3,6 +3,35 @@ import { pricesRepository } from "../repositories/index.js";
 class PriceService {
 	async getPrices() {
 		const prices = await pricesRepository.getPrices();
+
+		// convert prices dates toLocaleString 'es-AR'
+		const dateOptions = {
+			weekday: "short",
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+		};
+
+		prices.map((price) => {
+			price.date = new Date(price.date).toLocaleDateString(
+				"es-AR",
+				dateOptions
+			);
+		});
+
+		// convert prices toLocaleString 'es-AR'
+		const priceOptions = {
+			style: "currency",
+			currency: "ARS",
+			minimumFractionDigits: 2,
+		};
+
+		prices.map((price) => {
+			price.settle = new Intl.NumberFormat("es-AR", priceOptions).format(
+				price.settle
+			);
+		});
+
 		return prices;
 	}
 
