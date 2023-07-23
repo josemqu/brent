1babc2ac781b032dac62b2a8e594c007d0498bf5
José <mailjmq@gmail.com>
2023-07-23 00:20:59 -0300

    update price validation

--- src/public/js/prices.js
+++ src/public/js/prices.js
@@ -1,15 +1,21 @@
 console.log("prices.js");
 
-const runScrapingBtn = document.querySelector(".run-scraping");
-
-runScrapingBtn.addEventListener("click", () => {
+const runScraping = () => {
 	fetch("http://localhost:8080/api/v1/prices/scraped/price/").then((res) => {
 		res
 			.json()
 			.then((data) => {
 				console.log(data);
-				const response = createPrice(data);
-				return response;
+				if (data.dataType === "Final Data") {
+					const response = createPrice(data);
+					return response;
+				} else {
+					const errorMsg = {
+						result: "Price not created",
+						message: "The price is not final",
+					};
+					throw new Error(JSON.stringify(errorMsg));
+				}
 			})
 			.then((res) => {
 				if (res.ok) {
@@ -21,11 +27,11 @@ runScrapingBtn.addEventListener("click", () => {
 				}
 			})
 			.catch((err) => {
-				showAlert("error", err.result);
-				console.log(err);
+				const errorMsg = JSON.parse(err.message);
+				showAlert("error", errorMsg.result, errorMsg.message);
 			});
 	});
-});
+};
 
 const createPrice = async (price) => {
 	const response = await fetch("http://localhost:8080/api/v1/prices", {
