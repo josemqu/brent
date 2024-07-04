fa225f417fde14aabd9d8a66e8d830695591168f
josemqu <mailjmq@gmail.com>
2024-07-03 22:31:11 -0300

    puppeteer extra deleted

--- src/services/scraper.service.js
+++ src/services/scraper.service.js
@@ -1,4 +1,4 @@
-import puppeteer from "puppeteer-extra";
+import puppeteer from "puppeteer";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import AdblockerPlugin from "puppeteer-extra-plugin-adblocker";
 
